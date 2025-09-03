595976f87a03aaafe0632456375df099b452f22c
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-09-03 21:36:36 +0200

    Add initial setup

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Sheriff-Hoti/paper-tui/tui"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func main() {
-	fmt.Print("hi")
+	p := tea.NewProgram(tui.InitialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
